5d4daf6df32857ee1e23e8a326c0fb1fe21797f7
braum852 <julian.teo59@yahoo.com>
2022-04-21 01:03:57 +1000

    Working currency

--- src/components/Title.js
+++ src/components/Title.js
@@ -10,6 +10,7 @@ function App() {
 	const [amount2, setAmount2] = useState(1);
 	const [currency1, setCurrency1] = useState('AUD')
 	const [currency2, setCurrency2] = useState('USD')
+	const [rates, setRates] = useState([])
 
 	useEffect(() => {
 		axios.get('http://data.fixer.io/api/latest?access_key=81336ae3e06b60de326369e3f8927b24')
@@ -18,12 +19,36 @@ function App() {
 		})
 	}, []);
 
+	function format(number) {
+		return number.toFixed(4);
+	  }
+	
+	  function handleAmount1Change(amount1) {
+		setAmount2(format(amount1 * rates[currency2] / rates[currency1]));
+		setAmount1(amount1);
+	  }
+	
+	  function handleCurrency1Change(currency1) {
+		setAmount2(format(amount1 * rates[currency2] / rates[currency1]));
+		setCurrency1(currency1);
+	  }
+	
+	  function handleAmount2Change(amount2) {
+		setAmount1(format(amount2 * rates[currency1] / rates[currency2]));
+		setAmount2(amount2);
+	  }
+	
+	  function handleCurrency2Change(currency2) {
+		setAmount1(format(amount2 * rates[currency1] / rates[currency2]));
+		setCurrency2(currency2);
+	  }
+
 	
 		return (
 			<div>
 			  <h1>Currency Exchanger</h1>
-				<Currency onAmountChange={setAmount1} onCurrencyChange={setCurrency1} currencies={Object.keys(rates)} amount={amount1} currency={currency1}/>
-				<Currency onAmountChange={setAmount2} onCurrencyChange={setCurrency2} currencies={['USD']} amount={amount2} currency={currency2}/>
+				<Currency onAmountChange={handleAmount1Change} onCurrencyChange={setCurrency1} currencies={Object.keys(rates)} amount={amount1} currency={currency1}/>
+				<Currency onAmountChange={handleAmount2Change} onCurrencyChange={setCurrency2} currencies={Object.keys(rates)} amount={amount2} currency={currency2}/>
 			</div>
 		);
 }
