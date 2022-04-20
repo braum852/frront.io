9b463efd4e4067a7cb885e795efc824a1ced0ba5
braum852 <julian.teo59@yahoo.com>
2022-04-21 00:29:27 +1000

    API writeup start

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,21 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Currency from './Currency';
+import axios from 'axios';
 
 // APIKEY = 81336ae3e06b60de326369e3f8927b24
 
 function App() {
 
-	const [amount1, setAmount1] = useState(iniitialState: 1);
-	const [amount2, setAmount2] = useState(iniitialState: 1);
-	const [currency1, setCurrency1] = useState(iniitialState: 'AUD')
-	const [currency2, setCurrency2] = useState(iniitialState: 'USD')
+	const [amount1, setAmount1] = useState(1);
+	const [amount2, setAmount2] = useState(1);
+	const [currency1, setCurrency1] = useState('AUD')
+	const [currency2, setCurrency2] = useState('USD')
 
 	
 		return (
 			<div>
 			  <h1>Currency Exchanger</h1>
-				<Currency currencies={['AUD']} amount={amount1} currency={'currency1'}/>
-				<Currency currencies={['USD']} amount={amount2} currency={'currency2'}/>
+				<Currency currencies={['AUD']} amount={amount1} currency={currency1}/>
+				<Currency currencies={['USD']} amount={amount2} currency={currency2}/>
 			</div>
 		);
 }
